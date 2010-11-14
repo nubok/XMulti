f46a2bb4c3fdc306df1d988d761b2a002a88fdda
Wolfgang Keller <wolfgangkeller@gmail.com>
2010-11-15 00:34:22 +0100

    First implementation of a PHP backend.

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello, World";
+
+/*error_reporting(E_ALL);
+ini_set("display_errors", 1);*/
+
+//print_r(get_loaded_extensions());
+
+$document   = DOMDocument::load('blog.xml');
+$stylesheet = DOMDocument::load('blog.xslt');
+ 
+$processor = new XSLTProcessor;
+$processor->importStylesheet($stylesheet);
+ 
+print $processor->transformToDoc($document)->saveXML();
+
 ?>
